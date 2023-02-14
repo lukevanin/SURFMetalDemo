39995e7357caeb7914e5c43b8916b7f63d254da4
Luke Van In <lukevanin@gmail.com>
2023-02-14 12:03:26 +0200

    Initial Commit

---
+++ SURFMetalApp/SURFMetalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SURFMetalAppApp.swift
+//  SURFMetalApp
+//
+//  Created by Luke Van In on 2023/02/14.
+//
+
+import SwiftUI
+
+@main
+struct SURFMetalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
